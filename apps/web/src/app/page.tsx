5bc0880999130805c6bf1d124780df5ba032956f
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-30 12:43:05 -0500

    🏞️ include ether hook

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
+'use client'
+import { useAccount } from "@/hooks/ether";
+
+const Home = () => {
+  const { account, balance, contract, isOwner } = useAccount();
   return <main>Hello World</main>;
 }
+
+export default Home;
---
+++ apps/web/src/hooks/ether.ts
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { RecycleChain } from '../../../../standalone/recycle-chain-contract/typechain-types';
+import { ethers } from 'ethers';
+
+declare global {
+  interface Window {
+    ethereum: any
+  }
+}
+
+export const useAccount = () => {
+  const [account, setAccount] = useState('');
+  const [balance, setBalance] = useState('');
+  const [isOwner, setIsOwner] = useState(false);
+  const [contract, setContract] = useState<RecycleChain | null>(null);
+
+  const initializeWeb3Provider = async () => {
+    if (!window?.ethereum) {
+      alert('Non-Etherum browser detected. You should consider trying MetaMask!')
+      return
+    }
+
+    // const provider = new ethers.BrowserProvider(window.ethereum);
+
+    try {
+      await window.ethereum.request(
+        {
+          method: 'wallet_addEthereumChain',
+          params: [{
+            chainId: '0x13882', // NOTE: This is the hex rep. of 80002
+            chainName: 'Polygon Amoy Testnet',
+            nativeCurrentcy: {
+              name: 'MATIC',
+              symbol: 'MATIC',
+              decimals: 18,
+            },
+            blockExplorerUrls: ['https://amoy.polygonscan.com/'],
+            rls: [
+              `https://polygon-amoy.infure.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`,
+            ],
+          }],
+        }
+      );
+    } catch (error) {
+      console.error('User denied account access or failed to add network')
+    }
+  }
+  useEffect(() => {
+
+    // NOTE: initializeWeb3Provider
+
+    initializeWeb3Provider()
+    // NOTE: Fetch blockchain information
+  }, [])
+  return { account, balance, isOwner, contract }
+}

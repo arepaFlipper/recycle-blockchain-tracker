4871c1d02a3c214a77334c7eefe7e4aed41cf131
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-10-08 08:28:44 -0500

    🐟 render UserInfo

--- libs/ui/src/components/organisms/UserInfo.tsx
+++ libs/ui/src/components/organisms/UserInfo.tsx
@@ -1,14 +1,15 @@
+"use client";
 import { useAccount } from '@recycle-chain/util/src/hooks/ether';
 import { BaseComponent } from '@recycle-chain/util/src/types';
 import Image from 'next/image';
 
-export const UserInfo = ({ className, children }: BaseComponent) => {
+export const UserInfo = ({ className }: BaseComponent) => {
   const { account, balance, isOwner } = useAccount();
 
   return (
     <div className={className}>
       <div className="flex gap-3">
-        <Image src="/user-pattern.jpg" width={300} height={300} alt="" className="rounded-full w-16 h-16" />
+        <Image width={300} height={300} alt="" className="rounded-full w-16 h-16" src="/user-pattern.jpg" />
         <div>
           <div className="mb-2 pr-6">
             <span className="text-gray-600 break-all">{account}</span>
@@ -21,6 +22,9 @@ export const UserInfo = ({ className, children }: BaseComponent) => {
       </div>
       <div className="mt-2 bg-gray-25 p-2 text-right rounded">
         <div className="font-semibold">Balance</div>
+        <p className="text-gray-600 font-light text-2xl">
+          {Number(balance).toFixed(4)} MATIC
+        </p>
       </div>
     </div>
   )

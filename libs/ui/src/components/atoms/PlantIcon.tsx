e7dd0b00e090c52568a880ad34126fb9ddac0f53
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-10-08 09:07:54 -0500

    😆 Render logo in NavBar

--- libs/ui/src/components/atoms/PlantIcon.tsx
+++ libs/ui/src/components/atoms/PlantIcon.tsx
@@ -2,7 +2,13 @@ import { IconPlant } from '@tabler/icons-react'
 
 export const PlantIcon = () => {
   return (
-    <div className="flex items-center justify-center w-8 h-8 rounded-full shadow-lg cursor-pointer via-transparent bg-gradient-to-t from-green-50 to-transparent shadow-black/30">
+    <div
+      className="
+      flex items-center justify-center w-8 h-8 rounded-full shadow-lg
+      cursor-pointer via-transparent bg-gradient-to-t from-green-50
+      to-transparent shadow-black/30
+      "
+    >
       <IconPlant />
     </div>
   )

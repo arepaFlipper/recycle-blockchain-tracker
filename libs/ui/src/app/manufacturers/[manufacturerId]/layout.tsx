20444734553b606eb72c313b4da1b11e0517158d
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-10-08 11:23:53 -0500

    📦 display the manufacturer data

--- libs/ui/src/app/manufacturers/[manufacturerId]/layout.tsx
+++ libs/ui/src/app/manufacturers/[manufacturerId]/layout.tsx
@@ -1,12 +1,24 @@
+"use client"
+import { ManufacturerDocument } from "@recycle-chain/network/src/gql/generated";
+import { useQuery } from "@apollo/client";
+
 type LayoutProps = {
   children: React.ReactNode;
   params: { manufacturerId: string }
 }
 
-const layout = ({ children, params }: LayoutProps) => {
+const Layout = ({ children, params }: LayoutProps) => {
+  const { data } = useQuery(ManufacturerDocument, {
+    variables: { where: { id: params.manufacturerId } },
+  });
+  console.log(`🌖%clayout.tsx:14 - data`, 'font-weight:bold; background:#44bb00;color:#fff;'); //DELETEME:
+  console.log(data); // DELETEME:
   return (
-    <main>{children}</main>
+    <main>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {children}
+    </main>
   )
 }
 
-export default layout
+export default Layout

f750f9a1f4c202372c295ab92919583e98c3ca0f
Cristian F. Tovar <Christopher@Cristians-MacBook-Pro.local>
2024-10-10 21:35:48 -0500

    🔥 render a little piece of stake of Donut

--- libs/ui/src/components/organisms/CustomDonutChart.tsx
+++ libs/ui/src/components/organisms/CustomDonutChart.tsx
@@ -1,56 +1,60 @@
-import { pie, PieArcDatum, arc } from 'd3';
-import { useMemo } from 'react'
+import { pie, PieArcDatum, arc } from 'd3'
+import { useMemo, useState } from 'react'
 
-type TData = {
-  label: string;
-  value: number;
-  color: string;
+type DonutChartData = { label: string; value: number; color: string }
+
+interface IDonutChartProps {
+  data: DonutChartData[]
 }
 
-interface TDonutProps {
-  data: TData[];
-}
+const CustomDonutChart = ({ data }: IDonutChartProps) => {
+  const [hoveredValue, setHoveredValue] = useState<DonutChartData | null>(null)
 
-const CustomDonutChart = ({ data }: TDonutProps) => {
   const arcs = useMemo(() => {
-    const coverage = 0.66; // 0 to 1
-    const angle_coverage = Math.PI * coverage; // NOTE: full circle in radians
-    const start_angle = -angle_coverage / 2;
-    const end_angle = -angle_coverage / 2;
+    const coverage = 0.66 // 0 to 1
+    const angle_coverage = coverage * Math.PI * 2 // 2xPI is full circle.
+    const start_angle = -angle_coverage / 2
+    const end_angle = angle_coverage / 2
 
-    const calculateArcs = pie<TData>()
-      .value((data) => data.value).sort(null)
-      .startAngle(start_angle).endAngle(end_angle);
+    const calculateArcs = pie<DonutChartData>()
+      .value((data) => data.value)
+      .sort(null)
+      .startAngle(start_angle)
+      .endAngle(end_angle)
+      .padAngle(0.008)
 
-    console.log(`🧰 %cCustomDonutChart.tsx:25 - calculateArcs`, 'font-weight:bold; background:#679800;color:#fff;'); //DELETEME:
-    console.log(calculateArcs); // DELETEME:
+    const arcs = calculateArcs(data)
 
-    const calculatedArcs = calculateArcs(data);
-    console.log(`🐨%cCustomDonutChart.tsx:29 - calculatedArcs`, 'font-weight:bold; background:#718e00;color:#fff;'); //DELETEME:
-    console.log(calculatedArcs); // DELETEME:
+    console.log('arcs', arcs)
+    return arcs
+  }, [data])
 
-    return calculatedArcs;
-  }, [data]);
-  const diameter = 100;
-  const radius = diameter / 2;
-  const innerRadiusFactor = 0.6;
-  const innerRadius = radius * innerRadiusFactor;
-  const arcGenerator = arc<PieArcDatum<TData>>()
-    .innerRadius(innerRadius)
-    .outerRadius(radius);
+  const diameter = 100
+  const radius = diameter / 2
+  const inner_radiusFactor = 0.6
+  const inner_radius = radius * inner_radiusFactor
+  const strokeWidth = 0.5
 
-  console.log(`🌍%cCustomDonutChart.tsx:41 - arcGenerator`, 'font-weight:bold; background:#897600;color:#fff;'); //DELETEME:
-  console.log(arcGenerator); // DELETEME:
+  const arcGenerator = arc<PieArcDatum<DonutChartData>>()
+    .innerRadius(inner_radius)
+    .outerRadius(radius)
+    .cornerRadius(2)
+
+  console.log('arcGenerator ', arcGenerator)
 
   return (
     <div>
       <svg>
         <g>
-          {arcs.map((arc => <path key={arc.data.label} fill={arc.data.color} d={arcGenerator(arc) as string} />))}
+          {arcs.map((arc => {
+            return (
+              <path key={arc.data.label} fill={arc.data.color} d={arcGenerator(arc) || undefined} />
+            )
+          }))}
         </g>
       </svg>
     </div>
   )
 }
 
-export default CustomDonutChart
+export default CustomDonutChart;

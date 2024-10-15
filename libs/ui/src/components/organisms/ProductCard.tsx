811e2e88a1245e6c56c5287ff8d19f5ad99c6060
Cristian F. Tovar <Christopher@Cristians-MacBook-Pro.local>
2024-10-15 18:21:08 -0500

    🥦 add more info to the Manufacturer's product

--- libs/ui/src/components/organisms/ProductCard.tsx
+++ libs/ui/src/components/organisms/ProductCard.tsx
@@ -2,6 +2,8 @@ import { ProductsQuery } from "@recycle-chain/network/src/gql/generated";
 import { ReactNode } from "react";
 import DonutChartSimplified from "./DonutChartSimplified";
 import ToxicItemsChart from "./ToxicItemsChart";
+import Link from "next/link";
+import Styled from "../molecules/StyledLink";
 
 export interface IProductCardProps {
   product: NonNullable<ProductsQuery['products']>[0];
@@ -11,7 +13,7 @@ export interface IProductCardProps {
 
 const ProductCard = ({ product, children }: IProductCardProps) => {
   return (
-    <div key={product.id} className="p-4 shadow-lg overflow-hidden bg-white h-full rounded flex flex-col">
+    <div key={product.id} className="p-4 shadow-lg overflow-hidden bg-white h-full rounded flex flex-col text-gray-800">
       <DonutChartSimplified
         total={product.totalCount}
         sold={product.soldCount}
@@ -27,11 +29,24 @@ const ProductCard = ({ product, children }: IProductCardProps) => {
         <h2 className="font-medium text-gray-800">{product.manufacturer.name}</h2>
       </div>
 
-      <div className="flex gap-1 justify-center text-sm mt-2 text-gray">
+      <div className="flex gap-1 justify-center text-sm mt-2 ">
         <h2>{product.totalCount} items</h2>
       </div>
       <hr className="my-4" />
       <ToxicItemsChart toxicItems={product.toxicItems} />
+
+      <div className="mt-auto">
+        <div className="flex justify-end mt-4 ">
+          <Styled
+            href={`/manufacturers/${product.id}`}
+            className="underline underline-offset-4 font-semibold text-lg"
+            key={product.id}
+          >
+            View
+          </Styled>
+        </div>
+      </div>
+
     </div>
   );
 }

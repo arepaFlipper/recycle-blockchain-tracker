2ad56a12d917dff6e55202d34638c43a2d70e6f1
Cristian F. Tovar <Christopher@Cristians-MacBook-Pro.local>
2024-10-17 12:38:46 -0500

    🪐 modify the findOne manufacturer service

--- apps/api/src/models/manufacturers/graphql/manufacturers.service.ts
+++ apps/api/src/models/manufacturers/graphql/manufacturers.service.ts
@@ -7,13 +7,15 @@ import { PrismaService } from 'src/common/prisma/prisma.service'
 
 @Injectable()
 export class ManufacturersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
   findAll(args: FindManyManufacturerArgs) {
     return this.prisma.manufacturer.findMany(args)
   }
 
-  findOne(args: FindUniqueManufacturerArgs) {
-    return this.prisma.manufacturer.findUnique(args)
+  findOne({ where: { id } }: FindUniqueManufacturerArgs) {
+    return this.prisma.manufacturer.findFirst({
+      where: { id: { equals: id, mode: 'insensitive' } }
+    });
   }
 }

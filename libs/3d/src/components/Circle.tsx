f6016f340f496e895b890094161f39fa82a72b47
Cristian F. Tovar <Christopher@Cristians-MacBook-Pro.local>
2024-10-20 17:26:48 -0500

    🕊️ Match the 3D chart

--- libs/3d/src/components/Circle.tsx
+++ libs/3d/src/components/Circle.tsx
@@ -1,18 +1,20 @@
-import { Color, DoubleSide, Euler } from "three";
-import { radians } from '../util';
-import React from 'react';
+import { Color, DoubleSide, Euler } from 'three'
+import { radians } from '../util'
+import React from 'react'
 
-type CircleProps = {
-  distance?: number;
-  color?: Color;
-  rotation?: Euler;
-  size?: number;
-}
-
-const Circle = (props: CircleProps) => {
-  const { distance = 10, color = new Color('white'), rotation = new Euler(0, radians(0), 0), size } = props;
+const Circle = ({
+  distance = 10,
+  color = new Color('white'),
+  rotation = new Euler(0, radians(0), 0),
+  size = 0.25,
+}: {
+  distance?: number
+  color?: Color
+  rotation?: Euler
+  size?: number
+}) => {
   return (
-    <mesh position={[0, distance, 0]} rotation={rotation} >
+    <mesh position={[0, distance, 0]} rotation={rotation}>
       <circleGeometry attach="geometry" args={[size, 128]} />
       <meshBasicMaterial side={DoubleSide} attach="material" color={color} />
     </mesh>
--- libs/3d/src/scenes/SustainabilityScene.tsx
+++ libs/3d/src/scenes/SustainabilityScene.tsx
@@ -11,35 +11,58 @@ import Spawner from '../components/Spawner';
 import Circle from '../components/Circle';
 import { DURATION, SPAWN_INTERVAL } from '../util/constants';
 
-export const SustainabilityScene = ({ children, className = "" }: BaseComponent) => {
+const SustainabilityScene = ({ children, className = "" }: BaseComponent) => {
   return (
     <Canvas
       style={{
         zIndex: 0,
         height: `calc(100vh - 4rem)`,
-        background: 'linear-gradient(to top right, hsl(10,15%, 10%), hsl(10,3%, 5%)',
+        background: 'linear-gradient(to top right, hsl(10, 15%, 10%), hsl(10, 3%, 5%))',
       }}
     >
-      <Rotator speed={1.5}>
-        <Globe />
-        <group rotation={new Euler(radians(0), radians(-90), radians(90))}></group>
-        <Spawner
-          spawnInterval={SPAWN_INTERVAL * 1.25}
-          duration={DURATION * 1.5}
-          initialRotation={radians(240)}
-          endRotation={radians(-120)}
-          initialDelay={DURATION / 4}
-        >
-          <Circle distance={11} />
-        </Spawner>
-      </Rotator>
+
       <OrbitControls
         minPolarAngle={radians(0)}
-        maxPolarAngle={radians(180)}
+        maxPolarAngle={radians(120)}
         minDistance={60}
-        maxDistance={600}
+        maxDistance={800}
       />
-    </Canvas >
+      <Rotator>
+        <Globe />
+        <group rotation={new Euler(radians(0), radians(-90), radians(90))}>
+          <Spawner
+            spawnInterval={SPAWN_INTERVAL * 1.25}
+            duration={DURATION * 1.5}
+            initialRotation={radians(241)}
+            endRotation={radians(-120)}
+            initialDelay={DURATION / 4}
+          >
+            <Circle distance={11} />
+          </Spawner>
+          <Spawner
+            spawnInterval={SPAWN_INTERVAL * 1.25}
+            duration={DURATION * 1.25}
+            initialRotation={radians(361)}
+            endRotation={radians(0)}
+            initialDelay={DURATION / 4}
+          >
+            <Circle distance={10} />
+          </Spawner>
+          <Spawner
+            spawnInterval={SPAWN_INTERVAL * 1.25}
+            duration={DURATION * 1}
+            initialRotation={radians(121)}
+            endRotation={radians(-240)}
+            initialDelay={DURATION / 4}
+          >
+            <Circle distance={9} />
+          </Spawner>
+
+        </group>
+      </Rotator>
+    </Canvas>
   )
 
 }
+
+export default SustainabilityScene;

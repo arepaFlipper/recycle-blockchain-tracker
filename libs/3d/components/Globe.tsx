3bff7a6a06b72e3ca7b55efefbd08d72001c4ca6
Cristian F. Tovar <Christopher@Cristians-MacBook-Pro.local>
2024-10-20 05:40:11 -0500

    🌏 Render a world-wide globe

--- libs/3d/components/Globe.tsx
+++ libs/3d/components/Globe.tsx
@@ -1,9 +1,14 @@
 'use client'
 import React from 'react'
 import { Sphere } from "@react-three/drei"
+import { useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three'
 
 export const Globe = () => {
+  const texture = useLoader(TextureLoader, '/textures/earth_8k.jpeg')
   return (
-    <Sphere args={[1, 32, 32]} />
+    <Sphere args={[1, 32, 32]} >
+      <meshBasicMaterial map={texture} />
+    </Sphere>
   )
 }
